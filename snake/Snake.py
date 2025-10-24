19d689259475b5b251526fcba5713df57dca1d8c
Leon Mariotto <lmariott@student.42.fr>
2025-10-25 00:22:25 +0200

    Add 2 type of apple and reset game when loose.
    Add an internal reward variable.

--- snake/Snake.py
+++ snake/Snake.py
@@ -16,14 +16,19 @@ SCREEN_WIDTH, SCREEN_HEIGHT = CELL_SIZE * GRID_WIDTH, CELL_SIZE * GRID_HEIGHT
 # Colors
 COLORS = {
     "background": (30, 30, 30),
-    "snake": (0, 200, 0),
-    "food": (200, 0, 0),
+    "snake": (0, 0, 200),
+    "red_apple": (200, 0, 0),
+    "green_apple": (0, 200, 0),
     "grid": (40, 40, 40)
 }
 
 DIR_NAME = {(0,-1):"UP",(0,1):"DOWN",(-1,0):"LEFT",(1,0):"RIGHT"}
 
 class Snake():
+    """
+    A snake game. Provide functions to get the following game info :
+        - 
+    """
     def __init__(self, render:bool=False):
         # --- Initialize pygame ---
         self.render = render
@@ -32,16 +37,22 @@ class Snake():
             self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
             pygame.display.set_caption("Learn2Slither")
         # --- Game state ---
+        self.reset()
+        # print(self.__dict__)
+
+    def reset(self):
+        # --- Game state ---
         self.snake = [(5, 5), (4, 5), (3, 5)]
         self.direction = (1, 0)
-        self.food = (10, 10)
+        self.green_apple = [(5, 8), (8,5)]
+        self.red_apple = [(8,8)]
         self.head_textures = {
             "UP": Snake.make_head_texture("UP"),
             "DOWN": Snake.make_head_texture("DOWN"),
             "LEFT": Snake.make_head_texture("LEFT"),
             "RIGHT": Snake.make_head_texture("RIGHT"),
         }
-
+        # print(f"in reset: {self.__dict__}")
 
     def draw_grid(self):
         """Draw subtle grid lines."""
@@ -54,7 +65,7 @@ class Snake():
 
     # Create directional head textures dynamically
     @staticmethod
-    def make_head_texture(direction, color=(0,200,0)):
+    def make_head_texture(direction, color=(0,0,200)):
         surf = pygame.Surface((CELL_SIZE, CELL_SIZE), pygame.SRCALPHA)
         c = CELL_SIZE // 2
         if direction == "UP":
@@ -79,10 +90,10 @@ class Snake():
                 dir_name = DIR_NAME[self.direction]
                 self.screen.blit(self.head_textures[dir_name], rect.topleft)
             elif i == len(self.snake)-1:
-                pygame.draw.rect(self.screen, (0,150,0), rect, border_radius=10)
+                pygame.draw.rect(self.screen, (0,0,150), rect, border_radius=10)
             else:
-                pygame.draw.rect(self.screen, (0,180,0), rect, border_radius=4)
-                pygame.draw.rect(self.screen, (0,100,0), rect, width=2, border_radius=4)
+                pygame.draw.rect(self.screen, (0,0,180), rect, border_radius=4)
+                pygame.draw.rect(self.screen, (0,0,100), rect, width=2, border_radius=4)
 
     def draw(self):
         if self.render is False:
@@ -96,9 +107,11 @@ class Snake():
         #for (x, y) in self.snake:
         #    pygame.draw.rect(self.screen, COLORS["snake"], (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
     
-        # Draw food
-        fx, fy = self.food
-        pygame.draw.rect(self.screen, COLORS["food"], (fx * CELL_SIZE, fy * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        # Draw apple
+        for i,(x,y) in enumerate(self.red_apple):
+            pygame.draw.rect(self.screen, COLORS["red_apple"], (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        for i,(x,y) in enumerate(self.green_apple):
+            pygame.draw.rect(self.screen, COLORS["green_apple"], (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
         pygame.display.flip()
 
     def poll_input(self):
@@ -125,28 +138,61 @@ class Snake():
                 elif event.key == pygame.K_RIGHT and self.direction != (-1, 0):
                     self.direction = (1, 0)
 
+    def get_free_case(self):
+        # TODO do a condition of all case are taken
+        c = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+        while (c in self.red_apple) or (c in self.green_apple) or (c in self.snake):
+            c = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+        return c
+
+
     def move(self):
+
+        # print(f"in move {self.__dict__}\n")
         # Update snake
         head_x, head_y = self.snake[0]
         new_head = (head_x + self.direction[0], head_y + self.direction[1])
 
-        # Check bounds
-        if not (0 <= new_head[0] < GRID_WIDTH and 0 <= new_head[1] < GRID_HEIGHT):
-            # LOOSE
-            pygame.quit()
-            sys.exit()
+        # Default reward
+        self.reward = -1
 
-        # Check collision with snake
-        if new_head in self.snake:
+        # Check bounds
+        if (not (0 <= new_head[0] < GRID_WIDTH and 0 <= new_head[1] < GRID_HEIGHT)) or new_head in self.snake:
             # LOOSE
-            pygame.quit()
-            sys.exit()
+            self.reset()
+            self.reward = -20
+            return 
 
         # Move snake
+        increase_snake = False
         self.snake.insert(0, new_head)
-        if new_head == self.food:
-            # Place new food
-            while self.food in self.snake:
-                self.food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-        else:
+        for i,apple in enumerate(self.red_apple):
+            if new_head == apple :
+                self.reward = -5
+                self.red_apple[i] = self.get_free_case()
+                self.snake.pop()
+        for i,apple in enumerate(self.green_apple):
+            if new_head == apple :
+                self.reward = 5
+                self.green_apple[i] = self.get_free_case()
+                increase_snake = True
+
+        if increase_snake is False:
             self.snake.pop()
+
+        if (len(self.snake) == 0):
+            # LOOSE
+            self.reset()
+            self.reward = -20
+            return 
+
+
+    def get_state(self):
+        """
+        Get the snake vision: only column and row of snake head.
+        """
+
+    def get_reward(self):
+        """
+        Get the current reward: (calculated from last move)
+        """
